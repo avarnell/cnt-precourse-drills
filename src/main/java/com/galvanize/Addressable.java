09c3027e4e83bdcb31a8992a0ead771bef59b4ee
Andrew Varnell <avarnell@connectfirst.com>
2017-02-03 08:45:03 -0700

    Complete
    All tests pass + refactor

---
+++ src/main/java/com/galvanize/Addressable.java
@@ -0,0 +1,7 @@
+package com.galvanize;
+import java.util.List;
+
+interface Addressable {
+  public List<Address> getAddresses();
+  public void addAddress(Address newAddress);
+};
---
+++ src/main/java/com/galvanize/Business.java
@@ -0,0 +1,24 @@
+package com.galvanize;
+import java.util.*;
+
+public class Business implements Addressable{
+  private String name = "";
+  private List<Address> addresses = new ArrayList<>();
+
+  public Business(String name){
+    this.name =  name;
+  }
+
+  public List<Address> getAddresses(){
+    return addresses;
+  }
+
+  public void addAddress(Address newAddress){
+    addresses.add(newAddress);
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+}
